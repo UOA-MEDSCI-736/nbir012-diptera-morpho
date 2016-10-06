b4a5d649cb30f75c991fc7a598ea2985452bb532
Neil Birrell <nbir012@aucklanduni.ac.nz>
2016-10-06 15:26:46 +1300

    changed array name in L11 to tps_array, added notes for L15-17, moved L21 to L4.

--- Code/write-tps-to-csv.py
+++ Code/write-tps-to-csv.py
@@ -1,6 +1,7 @@
 import csv #importing the csv library into Python
 
 #this function writes processed arrays to csv
+path = "TPStoCSV.csv"
 def csv_writer(data2, path): #defining csv_writer function
     with open(path, 'w') as csv_file: 
         for line in data2:
@@ -12,13 +13,12 @@ tps_array = [] #create empty array
 with open('../Code/geo-morph-data.TPS','r') as file: #define file and open required tps file #I need to make this relative
     for line in file: #create loop using each line (string) within the file
         smallLine = line.strip('\r\n') #define smallLine by removing the newline fluff (\r\n)
-        a = smallLine.split(" ")
-        b = ",".join(map(str,a))
-        tps_array.append(b)
+        a = smallLine.split(" ")#this is splitting each string into 2 strings when there is a space
+        b = ",".join(map(str,a))#this is joining the two strings created in 'a' back together with a comma between them 
+        tps_array.append(b)#this is appending all the data in b into tps_array
 print(tps_array)
 
 #this defines what the file name will be.
-path = "TPStoCSV.csv"
 csv_writer(tps_array, path)
 
 # read csv
