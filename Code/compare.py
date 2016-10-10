46ea11087926971c9fc05db7ac65547abc0f02b8
Neil Birrell <nbir012@aucklanduni.ac.nz>
2016-10-11 11:43:42 +1300

    created test for tps csv comparison, created a file that compares csv array and tps array

---
+++ Code/compare.py
@@ -0,0 +1,31 @@
+array_tps = [] #create empty array
+with open('../Data/geo-morph-data.TPS','r') as file: #define file and open required tps file
+    for line in file: #create loop using each line (string) within the file
+        line = line.strip('\r\n') #define line by removing the newline fluff (\r\n)
+        line = line.strip(' ')
+        array_tps.append(line) #append (add) the outcome into the original blank array we created above.
+                
+values_tps = array_tps[0].split(' ') #defining values as individual components in the array split by blank space this takes the 0th one
+print(values_tps)    
+#if values[1] == '1672.00000':
+#    print('Test Passed')
+#else:
+#    print('Test Failed')
+    
+#print(values)
+
+array_csv = [] #create empty array
+with open('../Data/TPStoCSV.csv','r') as file: #define file and open required tps file
+    for line in file: #create loop using each line (string) within the file
+        #line = line.split(',') #define line by removing the newline fluff (\r\n)
+        line = line.strip('\r\n')
+        array_csv.append(line) #append (add) the outcome into the original blank array we created above.
+                
+values_csv = array_csv[0].split(',') #defining values as individual components in the array split by blank space this takes the 0th one
+print(values_csv)    
+#f values[1] == '1672.00000':
+#    print('Test Passed')
+#else:
+#    print('Test Failed')
+    
+#print(values)
---
+++ Code/test_tps_csv.py
@@ -0,0 +1,8 @@
+import unittest #importing library for unittests
+
+class compare_tps_csv(unittest.TestCase):
+
+    def test_a_equals_b(self):
+        compare_csv = Compare()
+        result = calc.add(2,2)
+        self.assertEqual(4, result)
