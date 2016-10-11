160102b628383f7762c4c2667f395c2455f20513
Neil Birrell <nbir012@aucklanduni.ac.nz>
2016-10-11 14:51:11 +1300

    arrays no longer match, I will need to fix

--- Code/compare.py
+++ Code/compare.py
@@ -1,3 +1,6 @@
+#class Compare(object):
+#    def compare(self, array_tps, array):
+#        pass
 array_tps = [] #create empty array
 with open('../Data/geo-morph-data.TPS','r') as file: #define file and open required tps file
     for line in file: #create loop using each line (string) within the file
@@ -5,8 +8,9 @@ with open('../Data/geo-morph-data.TPS','r') as file: #define file and open requi
         line = line.strip(' ')
         array_tps.append(line) #append (add) the outcome into the original blank array we created above.
                 
-values_tps = array_tps[0].split(' ') #defining values as individual components in the array split by blank space this takes the 0th one
-print(values_tps)    
+values_tps = array_tps[2].split(',') #defining values as individual components in the array split by blank space this takes the 0th one
+print(values_tps)
+#print(array_tps)    
 #if values[1] == '1672.00000':
 #    print('Test Passed')
 #else:
@@ -21,8 +25,9 @@ with open('../Data/TPStoCSV.csv','r') as file: #define file and open required tp
         line = line.strip('\r\n')
         array_csv.append(line) #append (add) the outcome into the original blank array we created above.
                 
-values_csv = array_csv[0].split(',') #defining values as individual components in the array split by blank space this takes the 0th one
-print(values_csv)    
+values_csv = array_csv[2].split(',') #defining values as individual components in the array split by blank space this takes the 0th one
+print(values_csv)
+#print(array_csv)    
 #f values[1] == '1672.00000':
 #    print('Test Passed')
 #else:
