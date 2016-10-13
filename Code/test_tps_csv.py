79c743b0cd9a879c5ebd985edff197aea03d16f0
Neil Birrell <nbir012@aucklanduni.ac.nz>
2016-10-13 13:57:45 +1300

    created several tests to compare different values between arrays

--- Code/test_tps_csv.py
+++ Code/test_tps_csv.py
@@ -1,8 +1,26 @@
 import unittest #importing library for unittests
+import compare as cp # importing my Compare() function
 
 class compare_tps_csv(unittest.TestCase):
 
-    def test_a_equals_b(self):
-        compare_csv = Compare()
-        result = calc.add(2,2)
-        self.assertEqual(4, result)
+    def test_compare_initial_index(self):
+        #compare = Compare()
+        #result = compare(array_tps,array_csv)
+        self.assertEquals(cp.array_csv[10], cp.array_tps[10])
+        
+    def test_compare_index_2(self):
+        #compare = Compare()
+        #result = compare(array_tps,array_csv)
+        self.assertEquals(cp.array_csv[2], cp.array_tps[2])
+    
+    def test_compare_final_index(self):
+        last_index = len(cp.array_csv) - 1
+        self.assertTrue(cp.array_csv[last_index] == cp.array_tps[last_index])
+
+    def test_compare_tricky_text(self):
+        tricky_index = len(cp.array_csv) - 2
+        self.assertTrue(cp.array_csv[tricky_index] == cp.array_tps[tricky_index])
+
+    def test_all_indices(self):
+        for index in range(len(cp.array_csv)):
+            self.assertEquals(cp.array_csv[index], cp.array_tps[index])
