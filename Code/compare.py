ec2a2b17daefa19f24f40714153b8620a07d340b
Neil Birrell <nbir012@aucklanduni.ac.nz>
2016-10-13 13:55:31 +1300

    removed superfluous code(most of it)

--- Code/compare.py
+++ Code/compare.py
@@ -1,6 +1,3 @@
-#class Compare(object):
-#    def compare(self, array_tps, array):
-#        pass
 array_tps = [] #create empty array
 with open('../Data/geo-morph-data.TPS','r') as file: #define file and open required tps file
     for line in file: #create loop using each line (string) within the file
@@ -9,36 +6,17 @@ with open('../Data/geo-morph-data.TPS','r') as file: #define file and open requi
         #line = line.split(' ')
         array_tps.append(line) #append (add) the outcome into the original blank array we created above.
                 
-#values_tps = array_tps[2].split(',') #defining values as individual components in the array split by blank space this takes the 0th one
-#print(values_tps)
-#print(array_tps)    
-#if values[1] == '1672.00000':
-#    print('Test Passed')
-#else:
-#    print('Test Failed')
-    
-#print(values)
 
 array_csv = [] #create empty array
 with open('../Data/TPStoCSV.csv','r') as file: #define file and open required tps file
     for line in file: #create loop using each line (string) within the file
-        #line = line.split(',') #define line by removing the newline fluff (\r\n)
         line = line.strip('\r\n')
         line = line.replace(",", " ")
         line = line.strip(" ")
         array_csv.append(line) #append (add) the outcome into the original blank array we created above.
                 
-#values_csv = array_csv[2].split(',') #defining values as individual components in the array split by blank space this takes the 0th one
-#print(values_csv)
-#print(array_csv)    
-#f values[1] == '1672.00000':
-#    print('Test Passed')
-#else:
-#    print('Test Failed')
-    
-#print(values)
+
 
 print(array_tps[1])
 print(len(array_csv))
-last_index = len(array_csv) - 1
 print(array_csv[2])
