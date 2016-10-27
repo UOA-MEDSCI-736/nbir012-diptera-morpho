d6fef16eed8956033595fc5c55f6f59b644fc5e1
Neil Birrell <nbir012@aucklanduni.ac.nz>
2016-10-27 23:57:53 +1300

    final code changes

--- Code/morphoManova.R
+++ Code/morphoManova.R
@@ -2,6 +2,17 @@
 morpho.df <- read.csv(file.choose(), header=T) 
 morpho.df <- morpho.df[,1:n]
 
+#assumption 1 -- check normality (linearity)
+library(mvnormtest)
+data.mat<- as.matrix(morpho.df)
+C <- t(morpho.df[1:n,1:n])
+mshapiro.test(R)
+
+#assumption 2 -- check equality of covariance matrices
+library(vegan)
+dist.mat <- dist(morpho.df)
+betadisper(dist.mat, group=as.factor(morpho.df$X))
+
 #morphoManova which runs a MANOVA on the data
 morphoManova <- manova ( cbind(Landmark1, Landmark2, Landmark3) ~ as.factor(X), data= morpho.df)
 summary(morphoManova)
