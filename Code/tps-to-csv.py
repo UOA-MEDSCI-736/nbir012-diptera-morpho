09b7833eb74a5b22741c4aee649514ea871fab96
Neil Birrell <nbir012@aucklanduni.ac.nz>
2016-10-28 19:09:59 +1300

    moving files in tidying up phase

--- Code/tps-to-csv.py
+++
@@ -1,6 +0,0 @@
-#test to see if file is defined. This test should fail.
-#file.read()
-#defining 'file' as a TPS file in my folder
-file = open('../Data/geo-morph-data.TPS','r')
-print(file.read())
-
